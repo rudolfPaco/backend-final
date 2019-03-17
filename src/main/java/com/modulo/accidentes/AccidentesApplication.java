1600cd0e80ab51367a4cc663594df2d74d92a1f9
Rudolf Felipez <rudolf.flodur@gmail.com>
2019-03-17 01:24:31 -0400

    inicio con el paquete model

---
+++ src/main/java/com/modulo/accidentes/AccidentesApplication.java
@@ -0,0 +1,13 @@
+package com.modulo.accidentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccidentesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccidentesApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/modulo/accidentes/model/empleado.java
@@ -0,0 +1,56 @@
+package com.modulo.accidentes.model;
+
+import javax.persistence.Entity;
+import java.util.Date;
+
+@Entity
+public class empleado {
+
+    private String nombre;
+    private String apellido;
+    private String cargo;
+    private String email;
+    private Date fechaContratacion;
+
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getFechaContratacion() {
+        return fechaContratacion;
+    }
+
+    public void setFechaContratacion(Date fechaContratacion) {
+        this.fechaContratacion = fechaContratacion;
+    }
+}
